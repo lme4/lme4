edb70a09da7d94c61229fb50f99a4c60dfaa75f0
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-20 11:12:23 +0000

    not using own error handler for now
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lme4/pkg/lme4@487 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/init.c
+++ src/init.c
@@ -34,17 +34,24 @@ static R_CallMethodDef CallEntries[] = {
   {NULL, NULL, 0}
 };
 
-/** cholmod_common struct local to the package */
+/** cholmod_common struct local to the lme4 package */
 cholmod_common c;
 
-/** Need our own CHOMOD error handler */
+/** Need our own CHOLMOD error handler */
 void attribute_hidden
 lme4_R_cholmod_error(int status, const char *file, int line, const char *message)
 {
     if(status < 0) {
+#ifdef Failure_in_matching_Matrix
+/* This fails unexpectedly with
+ *  function 'cholmod_l_defaults' not provided by package 'Matrix'
+ * from ../tests/lmer-1.R 's  (l.68)  lmer(y ~ habitat + (1|habitat*lagoon)
+ */
 	M_cholmod_defaults(&c);/* <--- restore defaults,
 				* as we will not be able to .. */
 	c.final_ll = 1;	    /* LL' form of simplicial factorization */
+#endif
+
 	error("Cholmod error '%s' at file:%s, line %d", message, file, line);
     }
     else
