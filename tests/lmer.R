79e9541fa61c461d3be16bcddfb75c4cc720e65b
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-12-11 21:54:01 +0000

    fix lmer.R tests - put both into lme4a & lme4
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lme4/pkg/lme4@1146 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/lmer.R
+++ tests/lmer.R
@@ -1,12 +1,21 @@
+require(lme4)
+source(system.file("test-tools.R", package = "Matrix"))# identical3() etc
+
 ## Check that quasi families throw an error
-try(gm1 <- lmer(cbind(incidence, size - incidence) ~ period + (1|herd),
-                data = cbpp, family = quasibinomial))
-try(gm1 <- lmer(incidence ~ period + (1|herd),
-                data = cbpp, family = quasipoisson))
-try(gm1 <- lmer(incidence ~ period + (1|herd),
-                data = cbpp, family = quasi))
+assertError(lmer(cbind(incidence, size - incidence) ~ period + (1|herd),
+		 data = cbpp, family = quasibinomial))
+assertError(lmer(incidence ~ period + (1|herd),
+		 data = cbpp, family = quasipoisson))
+assertError(lmer(incidence ~ period + (1|herd),
+		 data = cbpp, family = quasi))
 
-# check bug from Kevin Buhner
-X <- data.frame(y=runif(10), x=rnorm(10), z=sample(c("A","B"), 10, TRUE))
-lmer(log(y) ~ x | z, data=X)
+## check bug found by Kevin Buhner
+set.seed(7)
+n <- 10
+X <- data.frame(y=runif(n), x=rnorm(n), z=sample(c("A","B"), n, TRUE))
+fm <- lmer(log(y) ~ x | z, data=X)
+## gave error inside  model.frame()
+stopifnot(all.equal(unname(fixef(fm)), -0.8345, tol=.01))
 
+
+cat('Time elapsed: ', proc.time(),'\n') # for ``statistical reasons''
