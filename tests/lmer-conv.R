79b48d121e7a390bcc7a0fe767f2f6c8f9bf7d80
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-06 13:39:33 +0000

    add and more platform-info
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lme4/pkg/lme4@1340 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/lmer-conv.R
+++ tests/lmer-conv.R
@@ -2,10 +2,18 @@
 ##  ------------------
 ### The output of tests here are *not* 'diff'ed  (<==> no *.Rout.save file)
 library(lme4)
-sessionInfo()# { do *NOT* diff the output in this case! }
+## Platform - and other such info -- so we find it in old saved outputs
+SysI <- Sys.info()
+structure(Sys.info()[c(4,5,1:3)], class="simple.list")
+sessionInfo()
 ## and for even more details:
 c(Matrix = packageDescription("Matrix")$Built,
   lme4   = packageDescription("lme4")$Built)
+if(SysI[["sysname"]] == "Linux" && require("sfsmisc")) local({
+    nn <- names(.Sc <- sfsmisc::Sys.cpuinfo())
+    nn <- names(.Sc <- .Sc[nn != "flags"])
+    print(.Sc[grep("\\.[0-9]$", nn, invert=TRUE)])
+})
 
 ## convergence on boundary warnings
 load(system.file("external/test3comp.rda", package = "Matrix"))
