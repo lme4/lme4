5925f608136cc4adcbc4b16fd020fc910f19bdfa
dmbates <dmbates@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-14 22:00:54 +0000

    Modified link/muEta/variance unit tests to use a glmerResp object.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lme4/pkg/lme4Eigen@1371 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/unitTests/runit.link.R
+++ inst/unitTests/runit.link.R
@@ -1,16 +1,14 @@
 eps <- .Machine$double.eps
 oneMeps <- 1 - eps
 set.seed(1)
-etas <-
-    lapply(list(-8:8,             # equal spacing to asymptotic area
-                runif(20, -8, 8), # random sample from wide uniform dist
-                rnorm(20, 0, 8),  # random sample from wide normal dist
-                c(-10^30, rnorm(10, 0, 4), 10^30)), as.numeric)
-etapos <-
-    lapply(list(1:20,
-                rexp(20),
-                rgamma(20, 3),
-                pmax(eps, rnorm(20, 2, 1))), as.numeric)
+etas <- data.frame(A = seq.int(-8, 8, by=1),  # equal spacing to asymptotic area
+                   B = runif(17, -8, 8),  # random sample from wide uniform dist
+                   C = rnorm(17, 0, 8),   # random sample from wide normal dist
+                   D = c(-10^30, rnorm(15, 0, 4), 10^30))
+etapos <- data.frame(A = seq.int(1, 20, by=1),
+                     B = rexp(20),
+                     C = rgamma(20, 3),
+                     D = pmax(.Machine$double.eps, rnorm(20, 2, 1)))
 
 mubinom <-
     lapply(list(runif(100, 0, 1),
@@ -18,43 +16,50 @@ mubinom <-
                 pmin(pmax(eps, rbeta(100, 0.1, 3)), oneMeps),
                 pmin(pmax(eps, rbeta(100, 3, 0.1)), oneMeps)), as.numeric)
 
-tst.lnki <- function(fam, lst) {
-    ff <- glmFamily$new(fam)
-    unlist(lapply(lst, function(x)
-                  checkEquals(fam$linkinv(x), ff$linkInv(x))))
+tst.lnki <- function(fam, frm) {
+    rr <- glmerResp$new(fam, numeric(nrow(frm)))
+    sapply(frm, function(x) checkEquals(fam$linkinv(x), {rr$updateMu(x); rr$mu()}))
 }
-tst.link <- function(fam, lst) {
-    ff <- glmFamily$new(fam)
-    unlist(lapply(lst, function(x)
-                  checkEquals(fam$linkfun(x), ff$linkFun(x))))
+
+tst.muEta <- function(fam, frm) {
+    rr <- glmerResp$new(fam, numeric(nrow(frm)))
+    sapply(frm, function(x) checkEquals(fam$mu.eta(x), {rr$updateMu(x); rr$muEta()}))
 }
-tst.muEta <- function(fam, lst) {
-    ff <- glmFamily$new(fam)
-    unlist(lapply(lst, function(x)
-                  checkEquals(fam$mu.eta(x), ff$muEta(x))))
+
+tst.variance <- function(fam, frm) {
+    rr <- glmerResp$new(fam, numeric(nrow(frm)))
+    sapply(frm, function(x) checkEquals(fam$variance(fam$linkinv(x)), {rr$updateMu(x); rr$variance()}))
 }
 
 test.uncons.lnki <- function() {        # linkinv on unconstrained eta
     tst.lnki(binomial(), etas)          # binomial with logit link
     tst.muEta(binomial(), etas)
+    tst.variance(binomial(), etas)
     tst.lnki(binomial("probit"), etas)  # binomial with probit link
     tst.muEta(binomial("probit"), etas)
+    tst.variance(binomial("probit"), etas)
     tst.lnki(binomial("cloglog"), etas) # binomial with cloglog link
     tst.muEta(binomial("cloglog"), etas)
+    tst.variance(binomial("cloglog"), etas)
     tst.lnki(binomial("cauchit"), etas) # binomial with cauchit link
     tst.muEta(binomial("cauchit"), etas)
+    tst.variance(binomial("cauchit"), etas)
     tst.lnki(poisson(), etas)           # Poisson with log link
     tst.muEta(poisson(), etas)
+    tst.variance(poisson(), etas)
     tst.lnki(gaussian(), etas)          # Gaussian with identity link
     tst.muEta(gaussian(), etas)
+    tst.variance(gaussian(), etas)
 }
 
 test.pos.lnki <- function() {           # linkinv for positive eta
     set.seed(1)
     tst.lnki(Gamma(), etapos)           # gamma family
     tst.muEta(Gamma(), etapos)
+    tst.variance(Gamma(), etapos)
     tst.lnki(inverse.gaussian(), etapos) # inverse Gaussian
     tst.muEta(inverse.gaussian(), etapos)    
+    tst.variance(inverse.gaussian(), etapos)    
 }
 
 test.binom.link <- function() {         # link for binomial mu
