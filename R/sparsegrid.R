8fd9e57df6e759d10a472b0d11be2119423c88b7
dmbates <dmbates@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-31 18:21:24 +0000

    Simplify and document GQdk
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lme4/pkg/lme4Eigen@1536 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/sparsegrid.R
+++ R/sparsegrid.R
@@ -1,3 +1,17 @@
+##' Reproduce the sparse grid for Gauss-Hermite quadrature from stored values
+##'
+##' The GQN list of lists stores the condensed arrays from \url{www.sparse-grids.de}.
+##' This function recreates the full array by applying permutations and +/- multipliers.
+##' @title Multidimensional Gauss-Hermite sparse interpolation grid
+##' @param d dimension of grid
+##' @param k order of approximation.  The grid will provide accurate integrals for
+##'        polynomials of total order <= 2k - 1 multiplied by the kernel
+##' @return the grid as a matrix.  The first column contains the weights.  The
+##'        remaining d columns are the coordinates on the z scale.
+##' @author Douglas Bates
+##' @examples
+##' GQdk(3, 5)
+##' @export
 GQdk <- function(d=1L, k=1L) {
     stopifnot(0L < (d <- as.integer(d)[1]),
               d <= 20L,
@@ -5,7 +19,7 @@ GQdk <- function(d=1L, k=1L) {
               k <= length(GQNd <- GQN[[d]]))
     tmat   <- t(GQNd[[k]])
     dseq   <- seq_len(d)
-    rperms <- lapply(.Call(allPerm_int, dseq + 1L), function(v) c(1L, v))
+    rperms <- lapply(as.data.frame(.Call(allPerm_int, dseq + 1L)), function(v) c(1L, v))
     unname(unique(t(do.call(cbind,
                             lapply(as.data.frame(t(cbind(1,
                                                          as.matrix(do.call(expand.grid,
