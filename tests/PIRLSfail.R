20ecfed44ca5af77666c613c3825fd4c68334c7d
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-07 16:26:48 +0000

    - the deviance function at least is available - cosmetic
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lme4/pkg/lme4@1718 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/PIRLSfail.R
+++ tests/PIRLSfail.R
@@ -1,12 +1,20 @@
 ## example from HSAUR2 package
-## library("multcomp")
-## trees513 <- subset(trees513, !species %in% c("fir", "ash/maple/elm/lime", "softwood (other)"))
-## trees513$species <- trees513$species[,drop = TRUE]
-## levels(trees513$species)[nlevels(trees513$species)] <- "hardwood"
-## save("trees513",file="trees513.RData")
+if(FALSE) {
+    library("multcomp")
+    trees513 <- subset(trees513, !species %in%
+		       c("fir", "ash/maple/elm/lime", "softwood (other)"))
+    trees513$species <- trees513$species[,drop = TRUE]
+    levels(trees513$species)[nlevels(trees513$species)] <- "hardwood"
+    save("trees513", file="trees513.RData")
+}
+
 library("lme4")
 load("trees513.RData")
+dfun <- glmer(damage ~ species - 1 + (1 | lattice / plot),
+              data = trees513, family = binomial, devFunOnly = TRUE)
+ls.str(environment(dfun))# and you can investigate ...
+
 ## FIXME: try() to pass checks
-mmod <- try(lmer(damage ~ species - 1 + (1 | lattice / plot),
+mmod <- try(glmer(damage ~ species - 1 + (1 | lattice / plot),
               data = trees513, family = binomial()))
 
