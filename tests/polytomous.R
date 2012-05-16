fb2dd379d5d9166f00e6f74a3cba5965ae884950
bbolker <bbolker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-16 03:30:44 +0000

    added examples with compDev=FALSE, bobyqa
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lme4/pkg/lme4@1738 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/polytomous.R
+++ tests/polytomous.R
@@ -14,6 +14,18 @@ if (FALSE) {
     glmer(formula.poisson,data=data.poisson,family=poisson,verbose=10,optimizer="bobyqa")
     ## caught warning: maxfun < 10 * length(par)^2 is not recommended. -> infinite loop
 }
+## works but sloooow ....
+if (FALSE) {
+    try(g1 <- glmer(formula.poisson,data=data.poisson,family=poisson,
+                    compDev=FALSE,verbose=1))
+    ## runs for 2880 steps until:
+    ## Error in pp$updateDecomp() : Downdated VtV is not positive definite
+}
+glmer(formula.poisson,data=data.poisson,family=poisson,
+      compDev=FALSE,verbose=1,optimizer="bobyqa")
+## caught warning: maxfun < 10 * length(par)^2 is not recommended.
+## but runs to completion
+
 
 
 
