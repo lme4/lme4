86a4aa1b49d42fe7c15740df8e5e029e1a4b4a17
bbolker <bbolker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-03 13:53:21 +0000

    added respiratory test (works for Laplace, nAGQ={2,3,5}, fails in PIRLS otherwise)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lme4/pkg/lme4@1762 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ tests/respiratory.R
@@ -0,0 +1,9 @@
+## Data originally from Davis 1991 Stat. Med., as packaged in geepack
+## and transformed (center, id -> factor, idctr created, levels labeled)
+
+load("respiratory.RData")
+m_glmer_4.L <- glmer(outcome~center+treat+sex+age+baseline+(1|idctr),
+                     family=binomial,data=respiratory)
+## works for nAGQ={2,3,5}, fails otherwise
+m_glmer_4.GHQ8 <- glmer(outcome~center+treat+sex+age+baseline+(1|idctr),
+                        family=binomial,data=respiratory,nAGQ=5)
