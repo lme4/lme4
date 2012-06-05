5530a83624d4bb10faeb5006a65c4e791ec2437b
bbolker <bbolker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-05 16:20:31 +0000

    oops: forgot library("lme4") in test file
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/lme4/pkg/lme4@1763 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/respiratory.R
+++ tests/respiratory.R
@@ -1,5 +1,6 @@
 ## Data originally from Davis 1991 Stat. Med., as packaged in geepack
 ## and transformed (center, id -> factor, idctr created, levels labeled)
+library(lme4)
 
 load("respiratory.RData")
 m_glmer_4.L <- glmer(outcome~center+treat+sex+age+baseline+(1|idctr),
