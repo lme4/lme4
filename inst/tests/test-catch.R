eb5c0845fe2825a8bf54cd59952ba0723c8ce29a
bbolker <bbolker@gmail.com>
2012-11-17 18:31:10 -0500

    add basic tests for warnings

---
+++ inst/tests/test-catch.R
@@ -0,0 +1,10 @@
+library("testthat")
+library("lme4")
+
+context("storing warnings, convergence status, etc.")
+
+test_that("storewarning", {
+    expect_warning(glmer(cbind(incidence, size - incidence) ~ period + (1 | herd),
+          cbpp, binomial, control=list(maxfun=3),
+          optimizer="Nelder_Mead"),"failure to converge in 3")
+}
