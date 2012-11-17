72df49131481ba0bd6c170087c56cfb8de8c7c58
bbolker <bbolker@gmail.com>
2012-11-17 18:44:43 -0500

    oops, missing )

--- inst/tests/test-catch.R
+++ inst/tests/test-catch.R
@@ -7,4 +7,4 @@ test_that("storewarning", {
     expect_warning(glmer(cbind(incidence, size - incidence) ~ period + (1 | herd),
           cbpp, binomial, control=list(maxfun=3),
           optimizer="Nelder_Mead"),"failure to converge in 3")
-}
+})
