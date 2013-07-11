8d4437e1ebb01a96e81b0531ac981cdf27afb71c
bbolker <bbolker@gmail.com>
2013-07-11 09:52:41 -0400

    test from old r-sig-mixed thread

---
+++ tests/fewlevels.R
@@ -0,0 +1,31 @@
+## example from Gabor Grothendieck; didn't 
+## https://stat.ethz.ch/pipermail/r-sig-mixed-models/2010q2/003726.html
+## library(nlme)
+## set.seed(1)
+## f <- function(n, k) {
+##    set.seed(1)
+##    x <- 1:n
+##    fac <- gl(k, 1, n)
+##    fac.eff <- rnorm(k, 0, 4)[fac]
+##    e <- rnorm(n)
+##    y <- 1 + 2 * x + fac.eff + e
+##    lme(y ~ x, random = ~ 1 | fac)
+## }
+## n <- 10000
+sim <- function(n=1e4,k=4, seed=1) {
+    set.seed(seed)
+    x <- 1:n
+    fac <- gl(k, 1, n)
+    fac.eff <- rnorm(k, 0, 4)[fac]
+    e <- rnorm(n)
+    y <- 1 + 2 * x + fac.eff + e
+    data.frame(x,y,fac)
+}
+library(nlme)
+m.lme <- lme(y ~ x, random=~ 1|fac ,data=sim())
+v.lme <- as.numeric(VarCorr(m.lme)[1,1])
+library(lme4)
+m.lmer <- lmer(y ~ x + (1|fac),data=sim(),
+               control=lmerControl(check.numlev.gtreq.5="ignore"))
+v.lmer <- VarCorr(m.lmer)[[1]][1,1]
+stopifnot(all.equal(v.lmer,19.54829,tol=1e-6))
