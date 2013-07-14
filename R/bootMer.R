a20c50008fed55027ed80ac09505e6226ddebd64
bbolker <bbolker@gmail.com>
2013-07-14 12:20:34 -0400

    doc tweaks to bootMer docs (#46)
    export S3 method for as.data.frame.boot
    store 1000-rep boot sample in inst/testdata

--- R/bootMer.R
+++ R/bootMer.R
@@ -11,6 +11,18 @@
 ##    resampling the (centered!) residuals (for use.u=TRUE) or for use.u=FALSE,
 ##    *both* the centered u's + centered residuals
 ##    instead of using	rnorm()
+##  BUT see:
+## @article{morris_blups_2002,
+##	title = {The {BLUPs} are not "best" when it comes to bootstrapping},
+##	volume = {56},
+##	issn = {0167-7152},
+##	url = {http://www.sciencedirect.com/science/article/pii/S016771520200041X},
+##	doi = {10.1016/S0167-7152(02)00041-X},
+##	journal = {Statistics \& Probability Letters},
+##	author = {Morris, Jeffrey S},
+##	year = {2002},
+## }
+## for an indication of why this is not necessarily a good idea!
 
 ##' Model-based (Semi-)Parametric Bootstrap for Mixed Models
 ##'
@@ -20,14 +32,8 @@
 ##' provide a method for \code{\link{lmer}}  and \code{\link{glmer}} results.
 ##'
 ##' The working name for bootMer() was \dQuote{simulestimate()}, as it is an
-##' extension of \code{\link{simulate}}, but we rather want to emphasize its
+##' extension of \code{\link{simulate}}, but we want to emphasize its
 ##' potential for valid inference.
-##'
-##' In each of the \code{nsim} simulations --- that is what the
-##' \emph{parametric} bootstrap does, both \dQuote{\emph{spherized}} random
-##' effects \eqn{u} and the i.i.d errors \eqn{\epsilon} are generated, using
-##' \code{\link{rnorm}()} with parameters corresponding to the fitted model
-##' \code{x}.
 ##'
 ##' @param x fitted \code{*lmer()} model, see \code{\link{lmer}},
 ##'     \code{\link{glmer}}, etc.
@@ -36,27 +42,50 @@
 ##' @param nsim number of simulations, positive integer; the bootstrap \eqn{B}
 ##'     (or \eqn{R}).
 ##' @param seed optional argument to \code{\link{set.seed}}.
-##' @param use.u logical, indicating, if the spherized random effects should be
-##'     simulated / bootstrapped as well.  If \code{FALSE}, they are not changed,
-##'     and all inference is conditional on these.
+##' @param use.u logical, indicating whether the spherized random effects
+##' should be simulated / bootstrapped as well.  If \code{TRUE}, they are not
+##' changed, and all inference is conditional on these values. If \code{FALSE},
+##' new normal deviates are drawn (see Details).
 ##' @param type character string specifying the type of bootstrap,
-##'    \code{"parametric"} or \code{"semiparametric"}.  Default is
-##'    \code{"parametric"}.  Partial matching is allowed.
+##'    \code{"parametric"} or \code{"semiparametric"}; partial matching is allowed.
 ##' @param verbose logical indicating if progress should print output
-##' @param control an optional \code{\link{list}}, to be passed to the minimizer
-##'     (of the log-likelihood, or RE likelihood).
 ##' @param .progress character string - type of progress bar to
 ##'     display.  Default is \code{"none"}.
 ##' @param PBargs a list of additional arguments to the progress bar function.
 ##' @return an object of S3 \code{\link{class}} \code{"boot"}, compatible with
 ##'     \pkg{boot} package's \code{\link[boot]{boot}()} result.
-##' @seealso For inference, including confidence intervals,
+##' @seealso
+##' \itemize{
+##' \item For inference, including confidence intervals,
 ##'     \code{\link{profile-methods}}.
-##'
-##'     \code{\link[boot]{boot}()}, and then \code{\link[boot]{boot.ci}} from
+##' \item \code{\link[boot]{boot}()}, and then \code{\link[boot]{boot.ci}} from
 ##'     package \pkg{boot}.
-##' @references Davison, A.C. and Hinkley, D.V. (1997) \emph{Bootstrap Methods
+##' }
+##' @details
+##' \itemize{
+##' \item If \code{use.u} is \code{FALSE} and \code{type} is \code{"parametric"}, each simulation generates
+##' new values of both the \dQuote{\emph{spherized}} random
+##' effects \eqn{u} and the i.i.d. errors \eqn{\epsilon}, using
+##' \code{\link{rnorm}()} with parameters corresponding to the fitted model
+##' \code{x}.
+##' \item If \code{use.u} is {TRUE} and \code{type=="parametric"}
+##' semipar, only the i.i.d. errors
+##' (or, for GLMMs, response values drawn from the appropriate distributions)
+##' are resampled, with the values of \eqn{u} staying fixed at their
+##' estimated values.
+##' \item If \code{use.u} is {TRUE} and \code{type=="semiparametric"},
+##' the i.i.d. errors are sampled from the distribution of (response) residuals.
+##' (For GLMMs, the resulting sample will no longer have the same
+##' properties as the original sample, and the method may not make sense;
+##' a warning is generated.)
+##' \item The case where \code{use.u} is {FALSE} and \code{type=="semiparametric"} is not implemented; Morris (2002) suggests that resampling from the estimated values of \eqn{u} is not good practice.
+##' }
+##' @references
+##' \itemize{
+##' \item Davison, A.C. and Hinkley, D.V. (1997) \emph{Bootstrap Methods
 ##'     and Their Application}.  Cambridge University Press.
+##' \item Morris, J. S. (2002). The BLUPs Are Not \sQuote{best} When It Comes to Bootstrapping. \emph{Statistics & Probability Letters} 56(4): 425-430. doi:10.1016/S0167-7152(02)00041-X.
+##' }
 ##' @keywords models htest
 ##' @examples
 ##' fm01ML <- lmer(Yield ~ 1|Batch, Dyestuff, REML = FALSE)
@@ -64,30 +93,49 @@
 ##' mySumm <- function(.) { s <- sigma(.)
 ##'     c(beta =getME(., "beta"), sigma = s, sig01 = unname(s * getME(., "theta"))) }
 ##' (t0 <- mySumm(fm01ML)) # just three parameters
+##' ## alternatively:
+##' mySumm2 <- function(.) {
+##' c(beta=fixef(.),sigma=sigma(.),sig01=unlist(VarCorr(.)))
+##' }
 ##'
+##' set.seed(101)
 ##' ## 3.8s (on a 5600 MIPS 64bit fast(year 2009) desktop "AMD Phenom(tm) II X4 925"):
 ##' system.time( boo01 <- bootMer(fm01ML, mySumm, nsim = 100) )
 ##'
 ##' ## to "look" at it
-##' if(need.boot <- is.na(match("package:boot", search())))
-##'   require("boot")## is a recommended package, i.e. *must* be there
-##' boo01 # look at estimated bias for sig01 (-9.1, also when nsim = 1000)
+##' require("boot") ## a recommended package, i.e. *must* be there
+##' boo01
+##' ## note large estimated bias for sig01
+##' ## (~30% low, decreases _slightly_ for nsim = 1000)
 ##'
+##' ## extract the bootstrapped values as a data frame ...
+##' head(as.data.frame(boo01))
+##' 
 ##' ## ------ Bootstrap-based confidence intervals ------------
 ##'
+##' ## intercept
 ##' (bCI.1 <- boot.ci(boo01, index=1, type=c("norm", "basic", "perc")))# beta
 ##'
-##' ## Sigma - original scale, and interval calculation on log-scale:
+##' ## Residual standard deviation - original scale:
 ##' (bCI.2  <- boot.ci(boo01, index=2, type=c("norm", "basic", "perc")))
+##' ## Residual SD - transform to log scale:
 ##' (bCI.2l <- boot.ci(boo01, index=2, type=c("norm", "basic", "perc"),
 ##'                    h = log, hdot = function(.) 1/., hinv = exp))
 ##'
+##' ## Among-batch variance:
 ##' (bCI.3 <- boot.ci(boo01, index=3, type=c("norm", "basic", "perc")))# sig01
 ##'
+##' ## Graphical examination:
+##' plot(boo01,index=3)
+##'
+##' ## Check stored values from a longer (1000-replicate) run:
+##' load(system.file("testdata","boo01L.RData",package="lme4"))
+##' plot(boo01L,index=3)
+##' 
 ##' @export
 bootMer <- function(x, FUN, nsim = 1, seed = NULL, use.u = FALSE,
                     type=c("parametric","semiparametric"),
-		    verbose = FALSE, control = list(),
+		    verbose = FALSE, 
                     .progress="none", PBargs=list())
 {
     stopifnot((nsim <- as.integer(nsim[1])) > 0)
@@ -116,7 +164,7 @@ bootMer <- function(x, FUN, nsim = 1, seed = NULL, use.u = FALSE,
         ss <- simulate(x, nsim=nsim, use.u=use.u)
     } else {
         if (use.u) {
-            ## FIXME: does this work for GLMMs???
+            if (isGLMM(x)) warning("semiparametric bootstrapping is questionable for GLMMs")
             ss <- replicate(nsim,fitted(x)+sample(residuals(x,"response")),
                             simplify=FALSE)
         } else {
@@ -146,6 +194,7 @@ bootMer <- function(x, FUN, nsim = 1, seed = NULL, use.u = FALSE,
 	      class = "boot")
 } ## {bootMer}
 
+##' @S3method as.data.frame boot
 as.data.frame.boot <- function(x,...) {
   as.data.frame(x$t)
 }
