c5bc99992dda0eca5374f15f38977ffbabd10cf4
bbolker <bbolker@gmail.com>
2013-07-14 20:21:30 -0400

    test for #59

--- tests/testOptControl.R
+++ tests/testOptControl.R
@@ -1,9 +1,11 @@
 ## https://github.com/lme4/lme4/issues/59
 library(lme4)
 dat <- read.csv(system.file("testdata","dat20101314.csv",package="lme4"))
-library(lme4)
-
 NMcopy <- lme4:::Nelder_Mead
 
-fit <- lmer(y ~ (1|Operator)+(1|Part)+(1|Part:Operator), data=dat,
-        control=lmerControl("NMcopy", check.numlev.gtreq.5="ignore"))
+cc <- capture.output(lmer(y ~ (1|Operator)+(1|Part)+(1|Part:Operator), data=dat,
+        control=lmerControl("NMcopy", check.numlev.gtreq.5="ignore",
+        iprint=20)))
+## check that printing goes through step 140 twice and up to 240 once
+findStep <- function(str,n) sum(grepl(paste0("^\\(NM\\) ",n,": "),cc))
+stopifnot(findStep(cc,140)==2 && findStep(cc,240)==1)
