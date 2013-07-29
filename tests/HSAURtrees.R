847b08ac5c3d3879f73e54e353051ca34ba04f94
Martin Maechler <maechler@r-project.org>
2013-07-29 17:59:40 +0200

    if(FALSE) .. is no longer sufficient for CRAN checks

--- tests/HSAURtrees.R
+++ tests/HSAURtrees.R
@@ -1,18 +1,7 @@
 library("lme4")
 (testLevel <- if (nzchar(s <- Sys.getenv("LME4_TEST_LEVEL"))) as.numeric(s) else 1)
 
-## example from HSAUR2 package
- if(FALSE) {
-     ## (commented out to avoid R CMD check warning about undeclared dependency
-     library("multcomp")
-     trees513A <- droplevels(subset(trees513, !species %in%
-                        c("fir", "ash/maple/elm/lime", "softwood (other)")))
-     levels(trees513A$species)[nlevels(trees513A$species)] <- "hardwood"
-     trees513B <- droplevels(subset(trees513,
-                                    !species %in% c("fir", "softwood (other)")))
-     save("trees513A", "trees513B", file="trees513.RData")
- }
-
+## example from HSAUR2 package; data from 'multcomp'; see ../inst/testdata/trees513.R
 load(system.file("testdata","trees513.RData",package="lme4"))
 
 ## dfun <- glmer(damage ~ species - 1 + (1 | lattice / plot),
@@ -27,13 +16,13 @@ if (testLevel > 2) {
     ## oldres <- c(fixef(mmod0),getME(mmod0,"theta"))
     ## detach("package:lme4.0")
     ## dput(oldres)
-    oldres <- structure(c(5.23645064474105, 4.73568475545248, 2.65289926317093, 
+    oldres <- structure(c(5.23645064474105, 4.73568475545248, 2.65289926317093,
                           1.29043984816924, 1.59329381563025,
                           0.532663142106669, 1.16703186884403
                           ), .Names = c("speciesspruce", "speciespine",
-                             "speciesbeech", 
+                             "speciesbeech",
                              "speciesoak", "specieshardwood",
-                             "plot:lattice.(Intercept)", 
+                             "plot:lattice.(Intercept)",
                              "lattice.(Intercept)"))
     system.time(mmodA <- glmer(damage ~ species - 1 + (1 | lattice / plot),
                               data = trees513A, family = binomial()))
