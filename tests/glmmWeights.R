ebe52fd9aa6ad258defaa3801691f41f7ffdb834
Martin Maechler <maechler@r-project.org>
2013-08-05 17:53:55 +0200

    trying to use gSim() for producing data as  'issue #47' but I'm not there yet..

--- tests/glmmWeights.R
+++ tests/glmmWeights.R
@@ -4,7 +4,7 @@ library(testthat)
 source(system.file("testdata/lme-tst-funs.R", package="lme4", mustWork=TRUE))
 ##-> gSim(), a general simulation function ...
 
-## hand-coded Pearson residuals
+## hand-coded Pearson residuals {for  sumFun() }
 mypresid <- function(x) {
     mu <- fitted(x)
     w <- weights(x)
@@ -35,17 +35,45 @@ expect_equal(var(sumFun(g1)),0)
 
 
 ## POISSON
-g2 <-  glmer(y~x+(1|block),data=gSim(family=poisson()),
-             family=poisson)
+d.P <- gSim(family=poisson())
+g2 <-  glmer(y ~ x + (1|block),data = d.P, family=poisson)
 expect_equal(var(sumFun(g2)),0)
 
-d <- gSim(family=poisson())
-g2W <-  glmer(y~x+(1|block),data=d,
-             family=poisson,weights=rep(2,nrow(d)))
+g2W <- glmer(y ~ x + (1|block), data=d.P,
+             family=poisson, weights=rep(2,nrow(d.P)))
 expect_equal(var(sumFun(g2W)),0)
 ## correct
 
-g3 <-  glmer(y~x+(1|block),data=gSim(family=binomial(),nbinom=10),
+## non-Bernoulli BINOMIAL
+g3 <- glmer(y ~ x + (1|block),data=gSim(family=binomial(), nbinom=10),
+            family=binomial)
+expect_equal(var(sumFun(g3)),0)
+
+d.b.2 <- gSim(nperblk = 2, family=binomial())
+g.b.2 <- glmer(y ~ x + (1|block), data=d.b.2, family=binomial)
+expect_equal(var(sumFun(g.b.2)), 0)
+
+## Many blocks of only 2 observations each - (but nicely balanced)
+## Want this "as" https://github.com/lme4/lme4/issues/47
+## (but it "FAILS" survival already):
+##
+## n2 = n/2 :
+n2 <- 2048
+n2 <-  100 # for building/testing
+set.seed(47)
+dB2 <- gSim(n2, nperblk = 2, x= rep(0:1, each= n2), family=binomial())
+##                       --  --     ---  --------
+gB2 <- glmer(y ~ x + (1|block), data=dB2, family=binomial)
+
+## FAILS -----
+## library(survival)
+## (gSurv.B2 <- clogit(y ~ x + strata(block), data=dB2))
+## summary(gSurv.B2)
+## (SE.surf <- sqrt(diag(vcov(gSurv.B2))))
+
+
+
+g3 <-  glmer(y ~ x + (1|block),data=gSim(family=binomial(),nbinom=10),
              family=binomial)
 expect_equal(var(sumFun(g3)),0)
 
