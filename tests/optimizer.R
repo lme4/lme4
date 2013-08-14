fdb37baa5f55edf77546b7b10d0007d4d7a31738
Martin Maechler <maechler@r-project.org>
2013-08-13 23:04:35 -0600

    update to our own [g]lmerControl() calls

--- tests/optimizer.R
+++ tests/optimizer.R
@@ -11,17 +11,23 @@ fm1B <- lmer(Reaction ~ Days + (Days|Subject), sleepstudy,
              control=lmerControl(optimizer="bobyqa"))
 stopifnot(all.equal(fixef(fm1),fixef(fm1B)))
 require(optimx)
+lmerCtrl.optx <- function(method, ...)
+    lmerControl(optimizer="optimx", optControl=c(list(method=method), list(...)))
+glmerCtrl.optx <- function(method, ...)
+    glmerControl(optimizer="optimx", optControl=c(list(method=method), list(...)))
+
+
 ## FAILS on Windows (on r-forge only, not win-builder)... 'function is infeasible at initial parameters'
 ## (can we test whether we are on r-forge??)
 if (.Platform$OS.type != "windows") {
     fm1C <- lmer(Reaction ~ Days + (Days|Subject), sleepstudy,
-                 control=lmerControl(optimizer="optimx",method="nlminb"))
+                 control=lmerCtrl.optx(method="nlminb"))
     fm1D <- lmer(Reaction ~ Days + (Days|Subject), sleepstudy,
-                 control=lmerControl(optimizer="optimx", method="L-BFGS-B"))
+                 control=lmeroCtrl.optx(method="L-BFGS-B"))
     stopifnot(is.all.equal4(fixef(fm1),fixef(fm1B),fixef(fm1C),fixef(fm1D)))
 
     if (testLevel > 2) {
-        fm1E <- update(fm1,control=lmerControl(optimizer="optimx",method=c("nlminb","L-BFGS-B")))
+        fm1E <- update(fm1,control=lmerCtrl.optx(method=c("nlminb","L-BFGS-B")))
         ## hack equivalence of call and optinfo
         fm1E@call <- fm1C@call
         fm1E@optinfo <- fm1C@optinfo
@@ -33,12 +39,14 @@ if (.Platform$OS.type != "windows") {
 gm1 <- glmer(cbind(incidence, size - incidence) ~ period + (1 | herd),
              data = cbpp, family = binomial,
              control=glmerControl(tolPwrss=1e-13))
-gm1B <- update(gm1,control=glmerControl(tolPwrss=1e-13,optimizer="bobyqa"))
-gm1C <- update(gm1,control=glmerControl(optimizer="optimx",method="nlminb",tolPwrss=1e-13))
-gm1D <- update(gm1,control=glmerControl(optimizer="optimx",method="L-BFGS-B",tolPwrss=1e-13))
+gm1B <- update(gm1,control=glmerControl  (tolPwrss=1e-13, optimizer="bobyqa"))
+gm1C <- update(gm1,control=glmerCtrl.optx(tolPwrss=1e-13, method="nlminb"))
+gm1D <- update(gm1,control=glmerCtrl.optx(tolPwrss=1e-13, method="L-BFGS-B"))
 stopifnot(is.all.equal4(fixef(gm1),fixef(gm1B),fixef(gm1C),fixef(gm1D),tol=1e-5))
 
-gm1E <- update(gm1,control=glmerControl(optimizer="optimx",method=c("nlminb","L-BFGS-B"),tolPwrss=1e-13))
+gm1E <- update(gm1, control=glmerCtrl.optx(tolPwrss=1e-13,
+                    method=c("nlminb","L-BFGS-B")))
+
 
 if (testLevel > 2) {
     ## hack equivalence of call and optinfo
