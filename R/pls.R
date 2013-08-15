fa44f7ab8865ec680dd319abaa2a58649ef5e43f
Douglas Bates <dmbates@gmail.com>
2013-08-15 07:19:51 -0600

    Use a function closure.

--- R/pls.R
+++ R/pls.R
@@ -1,6 +1,6 @@
-##' Compute linear mixed model deviance
+##' Create linear mixed model deviance function
 ##'
-##' A pure \code{R} implementation of Doug Bates'
+##' A pure \code{R} implementation of the
 ##' penalized least squares (PLS) approach for computing
 ##' linear mixed model deviances. The purpose
 ##' is to clarify how PLS works without having
@@ -8,80 +8,79 @@
 ##' trying out modifications to PLS.
 ##'
 ##' @param obj output of \code{lFormula} or a model matrix
-##' @param theta covariance parameters
 ##' @param y response
 ##' @param weights prior weights
 ##' @param offset offset
 ##' @param REML calculate REML deviance?
 ##'
-##' @return deviance with a bunch of attributes:
-##' \itemize{
-##' \item[devML] ML deviance
-##' \item[devREML] REML deviance
-##' \item[beta] fixed effects at convergence
-##' \item[u] conditional mode at convergence
-##' \item[sigmaML] ML estimate of the residual standard deviation 
-##' \item[sigmaREML] REML estimate of the residual standard deviation
-##' }
+##' @return a function that evaluates the deviance or REML criterion
 ##' @export
 pls <- function(obj,theta,y,...) UseMethod("pls")
 
 ##' @rdname pls
 ##' @method pls matrix
 ##' @S3method pls matrix
-pls.matrix <- function(obj,theta,y,Zt,Lambdat,Lind,
+pls.matrix <- function(X,y,Zt,Lambdat,thfun,
                        weights = rep(1, length(y)),
                        offset = rep(0, length(y)),
                        REML = TRUE){
-    n <- nrow(obj)
-    p <- ncol(obj)
+    n <- length(y)
+    p <- ncol(X)
     q <- nrow(Zt)
-                                        # update Lambdat
-    Lambdat@x <- theta[Lind]
-                                        # calculate weighted design matrices
-    W <- Diagonal(x = sqrt(weights))
-    Ut <- Zt %*% W
-    LamtUt <- Lambdat %*% Ut
-    V <- W %*% obj
-                                        # calculate weighted response
-    wtResp <- as.vector(W %*% y)
-    ## sparse Cholesky decomposition of cross-product matrix
-    L <- Cholesky(tcrossprod(LamtUt), perm=FALSE, LDL = FALSE, Imult=1)
-    RZX <- solve(L, LamtUt%*%V, system = "L")
-    RX <- try(chol(crossprod(V) - crossprod(RZX)))
-    if(inherits(RX, "try-error")) return(NA)
-                                        # solve for fixed effect estimates and conditional modes
-    cu <- solve(L, LamtUt %*% wtResp, system = "L")
-    cb <- solve(t(RX), crossprod(V,wtResp) - crossprod(RZX,cu))
-    beta <- solve(RX, cb)
-    u <- solve(L, cu - RZX %*% beta, system = "Lt")
-                                        # calculate conditional mean of the response
-    mu <- crossprod(Zt,crossprod(Lambdat,u)) + (obj %*% beta) + offset
-                                        # calculate weighted residuals
-    wtres <- sqrt(weights)*(y-mu)
-                                        # calculate residual sums of squares
-    wrss <- sum(wtres^2)
-    pwrss <- wrss + sum(u^2)            # penalize
-                                        # calculate log determinants
-    ldL2 <- 2*determinant(L,logarithm=TRUE)$modulus
-    ldRX2 <- 2*determinant(RX,logarithm=TRUE)$modulus
-    attributes(ldL2) <- attributes(ldRX2) <- NULL
-                                        # calculate profiled deviance for theta
-    devML <- ldL2 + n*(1 + log(2*pi*pwrss) - log(n))
-    devREML <- ldL2 + ldRX2 + (n-p)*(1 + log(2*pi*pwrss) -
-                                     log(n-p))
-    if(REML) dev <- devREML
-    else dev <- devML
-    sigmaML <- sqrt(pwrss/sum(weights))
-    return(structure(dev, devML = devML, devREML = devREML, beta = beta, u = u,
-                     sigmaML = sigmaML,
-                     sigmaREML = sigmaML*(n/(n-p))))
+    stopifnot(nrow(X) == n,
+              ncol(Zt) == n,
+              nrow(Lambdat) == q,
+              ncol(Lambdat) == q,
+              is.function(thfun))
+                                        # calculate weighted products
+    W <- Diagonal(x = weights)
+    L <- Cholesky(tcrossprod(Lambdat %*% Zt), perm=FALSE, LDL = FALSE, Imult=1)
+    XtWX <- crossprod(X, W %*% X)
+    XtWy <- crossprod(X, W %*% y)
+    ZtWy <- Zt %*% (W %*% y)
+    ZtWX <- Zt %*% (W %*% X)
+    beta <- numeric(ncol(X))
+    u <- numeric(nrow(Zt))
+    mu <- numeric(nrow(X))
+    function(theta) {
+        Lambdat@x[] <- thfun(theta) 
+        L <- update(L, Lambdat %*% Zt, mult = 1)
+        cu <- solve(L, Lambdat %*% ZtWy, system = "L")
+        RZX <- solve(L, Lambdat %*% ZtWX, system = "L")
+        ## downdate XtWX and form Cholesky factor
+        DD <- as(XtWX - crossprod(RZX), "dpoMatrix")
+        ## conditional estimate of fixed-effects coefficients
+        beta <<- solve(DD, XtWy - crossprod(RZX, cu))
+        ## conditional mode of the spherical random-effects coefficients
+        u <<- solve(L, cu - RZX %*% beta, system = "Lt")
+                                        # conditional mean of the response
+        mu <<- crossprod(Zt,crossprod(Lambdat,u)) + X %*% beta + offset
+                                        # weighted residuals
+        wtres <- sqrt(weights)*(y-mu)
+                                        # weighted residual sums of squares
+        wrss <- sum(wtres^2)
+        pwrss <- wrss + sum(u^2)        # penalize
+                                        # log determinants
+        ldL2 <- 2*determinant(L,logarithm=TRUE)$modulus
+#        ldRX2 <- determinant(DD,logarithm=TRUE)$modulus # need method in Matrix
+        attributes(ldL2) <- NULL #attributes(ldRX2) <- NULL
+                                        # profiled deviance or REML criterion
+        ## if (REML) 
+        ##     ldL2 + ldRX2 + (n-p)*(1 + log(2*pi*pwrss) - log(n-p))
+        ## else
+        ldL2 + n*(1 + log(2*pi*pwrss) - log(n))
+    }
 }
 ##' @rdname pls
 ##' @method pls list
 ##' @S3method pls list
-pls.list <- function(obj,theta,y,weights=rep(1,length(y)),
+pls.list <- function(obj,y,weights=rep(1,length(y)),
                      offset=rep(0,length(y)),REML=TRUE) {
     retrm <- obj$reTrms
-    pls.matrix(obj$X,theta,y,retrm$Zt,retrm$Lambdat,retrm$Lind,weights,offset,REML)
+    thfun <- retrm$thfun
+    if (is.null(thfun)) thfun <- local({
+        Lind <- retrm$Lind
+        function(theta) theta[Lind]
+    })
+    pls.matrix(obj$X,y,retrm$Zt,retrm$Lambdat,thfun,weights,offset,REML)
 }
