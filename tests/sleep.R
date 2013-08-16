84567025ff5e0c7816137aec30ceea957cc84c48
Douglas Bates <dmbates@gmail.com>
2013-08-15 20:33:06 -0600

    Update man pages and tests.

--- tests/sleep.R
+++ tests/sleep.R
@@ -1,6 +1,8 @@
-library(lme4)
+library(lme4pureR)
 library(nloptwrap)
+library(lme4)
 lmod <- lFormula(Reaction ~ Days + (Days|Subject), sleepstudy)
+
 devf <- pls(lmod,sleepstudy$Reaction)
 bobyqa(c(1, 0, 1), devf, lower=c(0,-Inf,0))[c("par","value")]
 mML <- lmer(Reaction ~ Days + (Days|Subject),
