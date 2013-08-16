9c193d2a8fd5286408745b4ee8fc3bfe14266e85
Martin Maechler <maechler@r-project.org>
2013-08-16 17:54:48 -0600

    do need to work with lme4 {:-)}

--- tests/doubleVertNotation.R
+++ tests/doubleVertNotation.R
@@ -1,9 +1,4 @@
-# testing "||" notation for independent ranefs
-# setwd("C:/birs/lme4")
-# library(devtools)
-# rm(list=ls())
-# load_all()
-
+## testing "||" notation for independent ranefs
 
 library(lme4)
 library(testthat)
@@ -12,12 +7,12 @@ library(testthat)
 expect_equivalent(
   lFormula(Reaction ~ Days + (Days||Subject), sleepstudy)$reTrms,
   lFormula(Reaction ~ Days + (1|Subject) + (0 + Days|Subject), sleepstudy)$reTrms,
-)  
+)
 
 expect_equivalent(
   fitted(lmer(Reaction ~ Days + (Days||Subject), sleepstudy)),
   fitted(lmer(Reaction ~ Days + (1|Subject) + (0 + Days|Subject), sleepstudy)),
-)  
+)
 
 
 #works with nested
@@ -27,7 +22,7 @@ expect_equivalent(findbars(y ~ (x || id / id2)),
 #works with multiple
 expect_equivalent(findbars(y ~ (x1 + x2  || id / id2) + (x3 | id3) + (x4 || id4)),
                   findbars(y ~ (1 | id / id2) + (0 + x1 | id / id2) +
-                             (0 + x2 | id / id2) + (x3 | id3) + (1 | id4) + 
+                             (0 + x2 | id / id2) + (x3 | id3) + (1 | id4) +
                              (0 + x4| id4)))
 
 # leaves superfluous || alone:
@@ -42,8 +37,8 @@ expect_equivalent(findbars(y ~ (z + x)^2 + (x || id)),
 expect_equivalent(findbars(y ~ (x1*x2 || id)),
                   findbars(y ~ (1 | id) + (0+x1 | id) + (0 + x2 | id) +
                              (0 + x1:x2 | id)))
-      
+
 ## update now works as expected:
 (m <- lmer(Reaction ~ Days + (Days || Subject), sleepstudy))
 update(m, .~.-(0 + Days | Subject))
-   
+
