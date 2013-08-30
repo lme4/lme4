b2ad2e5071f5f741f3a5c1504a6408fb7cdf5a64
bbolker <bbolker@gmail.com>
2013-08-30 14:14:40 -0400

    fix ordering bug in bootMer/vcconv
    reported by Reinhold Kliegl
    applies for vector random effects with length>2

--- inst/tests/test-methods.R
+++ inst/tests/test-methods.R
@@ -9,3 +9,11 @@ test_that("lmer", {
     expect_warning(do.call(anova,list(fm0,fm1)),"assigning generic names")
 })
 
+context("bootMer")
+test_that("bootMer", {
+    ## testing bug-fix for ordering of sd/cor components in sd/cor matrix with >2 rows
+    m1 <- lmer(strength~1+(cask|batch),Pastes)
+    bb <- suppressWarnings(confint(m1,method="boot",nsim=3,quiet=TRUE))
+    corvals <- bb[grep("^cor_",rownames(bb)),]
+    expect_true(all(abs(corvals)<=1))
+})
