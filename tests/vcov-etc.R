af9bf9f24b22dfbd6e0d25fba948bdcd3a1c24d4
Martin Maechler <maechler@r-project.org>
2013-11-04 18:21:09 +0100

    more for the InstEval  data

--- tests/vcov-etc.R
+++ tests/vcov-etc.R
@@ -1,11 +1,13 @@
 stopifnot(require(lme4))
-library(lattice)
+
+(testLevel <- lme4:::testLevel())
+
 ## "MEMSS" is just 'Suggest' -- must still work, when it's missing:
 if (suppressWarnings(!require(MEMSS,quietly=TRUE)) ||
     (data(ergoStool, package="MEMSS") != "ergoStool")) {
+
     cat("'ergoStool' data from package 'MEMSS' is not available --> skipping test\n")
-    quit('no')
-}
+} else {
 
 fm1   <-  lmer (effort ~ Type + (1|Subject), data = ergoStool)
 fm1.s  <- lmer (effort ~ Type + (1|Subject), data = ergoStool, sparseX=TRUE)
@@ -14,8 +16,8 @@ fm1.s  <- lmer (effort ~ Type + (1|Subject), data = ergoStool, sparseX=TRUE)
 fe1   <- fixef(fm1)
 fe1.s <- fixef(fm1.s)
 
-s1.d <- summary(fm1)
-s1.s <- summary(fm1.s)
+print(s1.d <- summary(fm1))
+print(s1.s <- summary(fm1.s))
 stopifnot(
 	  all.equal(fe1, fe1.s, tol= 1e-12
 	  ),
@@ -32,16 +34,17 @@ stopifnot(
                     tol = 1.e-8)
           )
 
+}## if( ergoStool is available from pkg MEMSS )
+
 ### -------------------------- a "large" example -------------------------
 str(InstEval)
 
-if (FALSE) {                            # sparse X is not currently implemented
-## this works
-system.time(
+if (FALSE) { # sparse X is not currently implemented, so forget about this:
+
+system.time(## works with 'sparseX'; d has 1128 levels 
 fm7 <- lmer(y ~ d + service + studage + lectage + (1|s),
              data = InstEval, sparseX=TRUE, verbose=1L, REML=FALSE)
 )
-if (nchar(Sys.getenv("_LME4_LONG_TESTS_")) > 0) {
 system.time(sfm7 <- summary(fm7))
 fm7 # takes a while as it computes summary() again !
 
@@ -54,19 +57,46 @@ stopifnot(all.equal(m.t.7, 1.56642013605506, tol = 1.e-6)) ## ML
 hist.t <- cut(t.fm7, floor(min(t.fm7)) : ceiling(max(t.fm7)))
 cbind(table(hist.t))
 
-system.time(fm8 <-
-            lmer(y ~ service * dept + studage + lectage +
-                 (1|s) + (1|d), InstEval, verbose = 1L, REML=FALSE))
-fm8
+}# fixed effect 'd' -- with 'sparseX' only --------------------------------
 
-system.time(fm9 <-
-            lmer(y ~ studage + lectage +
-                 (1|s) + (1|d) + (1|dept:service) + (1|dept),
-                 InstEval, verbose = 1L, REML=FALSE))
+if(testLevel <= 1) { cat('Time elapsed: ', proc.time(),'\n'); q("no") }
+
+## ELSE : (testLevel > 1) :
+library(lattice)
+
+system.time(
+    fm8 <- lmer(y ~ service * dept + studage + lectage +
+                (1|s) + (1|d), InstEval, verbose = 1L, REML=FALSE)
+    ) ## 62 secs [MM@lynne; 2013-11]
+fm8
+sm8 <- summary(fm8)
+str(r8 <- ranef(fm8))
+sapply(r8, summary)
+r.m8 <- cov2cor(vcov(sm8))
+Matrix::image(r.m8, main="cor(<fixed eff[ lmer(*,InstEval) ]>)")
+
+if(testLevel <= 2) { cat('Time elapsed: ', proc.time(),'\n'); q("no") }
+
+## ELSE: testLevel > 2
+
+## Clearly smaller X, but more RE pars
+## ==> currently considerably slower than the above
+
+system.time(
+    fm9 <-
+    lmer(y ~ studage + lectage +
+         (1|s) + (1|d) + (1|dept:service) + (1|dept),
+         InstEval, verbose = 1L, REML=FALSE)
+    ) ## 410 secs [MM@lynne; 2013-11]
+
 fm9
-rr <- ranef(fm9, condVar = TRUE)
-qqmath(rr,strip=FALSE)$d
+(sm9 <- summary(fm9))
+rr <- ranef(fm9, condVar = TRUE) ## ~ 10 secs
+
+sapply(rr, summary)
+qqr <- qqmath(rr, strip=FALSE)
+qqr$d
+qqr$s
 dotplot(rr,strip=FALSE)$`dept:service`
-}
-}
+
 cat('Time elapsed: ', proc.time(),'\n') # for ``statistical reasons''
