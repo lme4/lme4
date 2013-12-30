8062ebb843ad1b52b903b13c3955f9feedd8a852
Martin Maechler <maechler@r-project.org>
2013-12-30 19:38:21 +0100

    update: including  Nelder_Mead  vs  bobyqa   comparison (small differ. here)

--- misc/issues/rank-32bit-stla-ex.R
+++ misc/issues/rank-32bit-stla-ex.R
@@ -1,24 +1,10 @@
 #### https://github.com/lme4/lme4/issues/162
 
-dat <- data.frame(sample = structure(c(1L, 1L, 1L, 1L, 1L, 1L, 1L,
-1L, 1L, 1L, 1L, 1L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L,
-2L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 4L, 4L, 4L,
-4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 5L, 5L, 5L, 5L, 5L, 5L, 5L,
-5L, 5L, 5L, 5L, 5L, 6L, 6L, 6L, 6L, 6L, 6L, 6L, 6L, 6L, 6L, 6L,
-6L), .Label = c("10", "100", "1000", "10000", "100000", "1000000"
-), class = "factor"),
-                  operator = structure(c(1L, 1L, 1L, 1L,
-1L, 1L, 2L, 2L, 2L, 2L, 2L, 2L, 1L, 1L, 1L, 1L, 1L, 1L, 2L, 2L,
-2L, 2L, 2L, 2L, 1L, 1L, 1L, 1L, 1L, 1L, 2L, 2L, 2L, 2L, 2L, 2L,
-1L, 1L, 1L, 1L, 1L, 1L, 2L, 2L, 2L, 2L, 2L, 2L, 1L, 1L, 1L, 1L,
-1L, 1L, 2L, 2L, 2L, 2L, 2L, 2L, 1L, 1L, 1L, 1L, 1L, 1L, 2L, 2L,
-2L, 2L, 2L, 2L), .Label = c("AVR", "SCF"), class = "factor"),
-                  day = structure(c(1L, 1L, 2L, 2L, 3L, 3L, 1L, 1L, 2L, 2L,
-    3L, 3L, 1L, 1L, 2L, 2L, 3L, 3L, 1L, 1L, 2L, 2L, 3L, 3L, 1L,
-    1L, 2L, 2L, 3L, 3L, 1L, 1L, 2L, 2L, 3L, 3L, 1L, 1L, 2L, 2L,
-    3L, 3L, 1L, 1L, 2L, 2L, 3L, 3L, 1L, 1L, 2L, 2L, 3L, 3L, 1L,
-    1L, 2L, 2L, 3L, 3L, 1L, 1L, 2L, 2L, 3L, 3L, 1L, 1L, 2L, 2L,
-    3L, 3L), .Label = c("1", "2", "3"), class = "factor"),
+dat <- data.frame(sample   = gl(6, 12,    labels=formatC(10^(1:6), format="d")),
+                  operator = gl(2, 6, 72, labels=c("AVR","SCF")),
+                  day      = gl(3, 2, 72),
+### MM: The '-Inf' below shows that  log10(y)  below is really "wrong";
+                  ## OTOH, the example is even slightly more strange then:
                   y = c(1.12456538692733,
     1.04023576431277, 1.12910081120271, 0.581806780532349, 1.07647980145301,
     1.22161328950346, 1.51177691237896, -Inf, 1.19015015315616,
@@ -38,13 +24,12 @@ dat <- data.frame(sample = structure(c(1L, 1L, 1L, 1L, 1L, 1L, 1L,
     5.61555385330924, 5.82949401647961, 5.86877631695349, 5.88521433806452,
     5.85461376554296, 5.9006285826237, 6.07374763216832, 6.09786080600134,
     5.98030329499588, 5.89387938834901, 5.92806626774672))
-
 str(dat)
 ## 'data.frame':	72 obs. of  4 variables:
 ##  $ sample  : Factor w/ 6 levels "10","100","1000",..: 1 1 1 1 1 1 1 1 1 1 ...
 ##  $ operator: Factor w/ 2 levels "AVR","SCF": 1 1 1 1 1 1 2 2 2 2 ...
 ##  $ day     : Factor w/ 3 levels "1","2","3": 1 1 2 2 3 3 1 1 2 2 ...
-##  $ y       : num  1.125 1.04 1.129 0.582 1.076 ...
+##  $ y       : num  3.12 2.88 3.12 1.75 2.88 ...
 
 ## Completely balanced:
 ftable( xtabs(~operator+sample+day, dat) )
@@ -65,7 +50,7 @@ ftable( xtabs(~operator+sample+day, dat) )
 
 require(lattice)
 splom(~ dat) ##--> clear picture  y ~ as.numeric(sample)
-xyplot(y ~ log10(as.numeric(as.character(sample))), dat)
+xyplot(log10(y) ~ log10(as.numeric(as.character(sample))), dat)
 
 ## Given the above picture, it seems *very*  "adventurous" to take
 ## 1) 'sample' as random effect factor; rather log10(..) should be used as *numeric* fixed effect
@@ -76,14 +61,28 @@ library(lme4)
 fitAV3 <- lmer(log10(y) ~ (1|sample)+(1|day)+(1|operator)+
                (1|day:sample)+(1|day:operator)+(1|sample:operator)+
                (1|day:sample:operator),
-               data=subset(dat,y>0) )
+               control = lmerControl("Nelder_Mead"),
+               data = subset(dat, y > 0) )
 ## No longer seeing this:
 ##  Warning message:
 ## In checkZrank(reTrms$Zt, n = n, control, nonSmall = 1e+06) :
 ##   number of observations <= rank(Z); variance-covariance matrix will be unidentifiable
-
 ## MM: No longer happens with new  Matrix 1.1-1 (Dec.21, 2013)
+
+## but with new checks, now get
+## Warning message:
+## In checkConv(attr(opt, "derivs"), opt$par, checkCtrl = control$checkConv,  :
+##   Model failed to converge with max|grad| = 0.00122188 (tol = 0.001)
+
+f.tAV3 <- lmer(log10(y) ~ (1|sample)+(1|day)+(1|operator)+
+               (1|day:sample)+(1|day:operator)+(1|sample:operator)+
+               (1|day:sample:operator),
+               control = lmerControl("bobyqa"),
+               data = subset(dat, y > 0) )
+## no warning
+
 summary(fitAV3) ## looks "fine"
+summary(f.tAV3) ## ditto; but better: the three sigma's are = 0, exactly
 
 myfile <- "fit+pr_AV3.rda"
 if(file.exists(myfile)) print(load(myfile)) else {
@@ -103,20 +102,39 @@ if(file.exists(myfile)) print(load(myfile)) else {
     ##  2013-12-28 [nb-mm3]:
     ##    user  system elapsed
     ## 139.352   0.112 139.943
-    save(fitAV3, pr.AV3, file=myFile)
+    ##  2013-12-30 [nb-mm3]: with new 'check-conv': 15% slower:
+    ##    user  system elapsed
+    ## 161.028   0.012 161.569
+
+    system.time(
+        p..AV3 <- profile(f.tAV3)
+        ## Warning messages:
+        ## 1: In profile.merMod(f.tAV3) : non-monotonic profile
+        ## 2: In profile.merMod(f.tAV3) : non-monotonic profile
+        ## 3: In profile.merMod(f.tAV3) : non-monotonic profile
+        )
+    ##	2013-12-30 [nb-mm3]: with new 'check-conv':
+    ##    user  system elapsed
+    ## 147.296   0.000 147.788
+
+    save(fitAV3, f.tAV3, pr.AV3, p..AV3,
+	 file=myfile)
 }
 
-confint(pr.AV3)
+confint(pr.AV3) # (prettied)
 ##                  2.5 %     97.5 %
 ## .sig01      0.01127495 0.05807177
-## .sig02      0.00000000        Inf
-## .sig03      0.00000000        Inf
-## .sig04      0.00000000 0.05530778
+## .sig02      0.                Inf
+## .sig03      0.                Inf
+## .sig04      0.         0.05530778
 ## .sig05      0.16340194 0.53108204
-## .sig06      0.00000000 0.08005722
-## .sig07      0.00000000        Inf
+## .sig06      0.         0.08005722
+## .sig07      0.                Inf
 ## .sigma      0.03406133 0.05606905
 ## (Intercept) 0.21048244 0.71334815
+
+stopifnot(all.equal(confint(pr.AV3),
+                    confint(p..AV3), tol= 5e-5))## 64bit: see 2.43e-05
 
 ##--> hmm... want to have a *match* between all the  ".sig<n>" and the "group" names,
 ## see vc3 below (etc)
@@ -129,12 +147,12 @@ noquote(cbind(lme4:::formatVC(vc3, formatter=formatC),
 ##                                                               __manually__
 ##  Groups              Name         Std.Dev.   2.5 %   97.5 %   name{profile}
 ##  day:sample:operator (Intercept)  0.036138   0.01127 0.05807   .sig01
-##  day:sample          (Intercept)  8.6908e-07 0           Inf   .sig02
-##  sample:operator     (Intercept)  2.0933e-06 0           Inf   .sig03
+##  day:sample          (Intercept)  7.3444e-07 0           Inf   .sig02
+##  sample:operator     (Intercept)  2.1048e-06 0           Inf   .sig03
 ##  day:operator        (Intercept)  0.016983   0       0.05531   .sig04
 ##  sample              (Intercept)  0.28951    0.1634  0.53108   .sig05
 ##  day                 (Intercept)  0.009331   0       0.08006   .sig06
-##  operator            (Intercept)  1.1809e-06 0           Inf   .sig07
+##  operator            (Intercept)  1.1641e-06 0           Inf   .sig07
 ##  Residual                         0.042778   0.03406 0.05607   .sigma
 
 ## FIXME:
