50dcd31b21954ccf146839644dfb08b285221c83
bbolker <bbolker@gmail.com>
2014-06-19 19:32:47 -0400

    squash vignette more

--- misc/cp_vignette.R
+++ misc/cp_vignette.R
@@ -1,9 +1,9 @@
 paper.dir <- "~/R/lme4_misc/papers/lmer_JSS"
+## assume we start in misc
 vignette.dir <- "../vignettes"
 setwd(vignette.dir)
 system(sprintf("sed -e \"s/\\\\documentclass{jss}/\\documentclass[nojss]{jss}/\" -e \"s/%%VIGNETTE_NOTE/Submitted to \\\\\\emph{Journal of Statistical Software}/\" %s/lmer.Rnw >%s/lmer.Rnw",paper.dir,"."))
-library("knitr")
-knit2pdf("lmer.Rnw")
-tools::compactPDF("lmer.pdf")
+knitr::knit2pdf("lmer.Rnw")
+tools::compactPDF("lmer.pdf",gs_quality="ebook")
 file.copy("lmer.pdf","../inst/doc",overwrite=TRUE)
 unlink("lmer.pdf")
