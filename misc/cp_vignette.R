ee7ddc9a95deb85a86ea47a6266742762531e3d6
Martin Maechler <maechler@r-project.org>
2014-06-22 00:48:02 +0200

    * inst/doc/lmer.pdf: update from bitbucket

--- misc/cp_vignette.R
+++ misc/cp_vignette.R
@@ -12,10 +12,20 @@ if(!file.exists(lmerSrc)) {
     system(sprintf("sed -e \"s/\\\\documentclass{jss}/\\documentclass[nojss]{jss}/\" -e \"s/%%VIGNETTE_NOTE/Submitted to \\\\\\emph{Journal of Statistical Software}/\" %s > lmer.Rnw",
                    lmerSrc))
     knitr::knit2pdf("lmer.Rnw")
+    ## for arXiv:
+    if(!file.exists("jss.cls"))
+        file.symlink(file.path(R.home(), "share/texmf/tex/latex", "jss.cls"), ".")
+    ## unfortunately, R's tar() is not usable for this ... {aargh!}
+    ## tar("lmer_4_arxiv.tar",
+    ##     files = c("lmer.tex", "lmer.bbl", "figure", "jss.cls"))
+    system("tar cfzh lmer_4_arxiv.tar.gz lmer.tex lmer.bbl figure jss.cls figure")
+    untar("lmer_4_arxiv.tar.gz", list=TRUE)
+    ##
     tools::compactPDF("lmer.pdf",gs_quality="ebook")
     file.copy("lmer.pdf","../inst/doc", overwrite=TRUE)
     ls.lmer <- function() list.files(pattern="^lmer[.]...$")
     fls <- local({L <- ls.lmer(); L[!(L %in% paste0("lmer.", c("bib", "Rnw")))]})
+    cat("--> tar for arXiv:  ", list.files(pattern="^lmer_4_arxiv"),"\n")
     cat("removing ",fls,": .."); unlink(fls); cat("\n")
     cat(" --> lmer.* in vignettes:\n"); print(ls.lmer())
     setwd(owd)# back
