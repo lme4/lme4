ad66171501f501dd635167b9b0f8ce0f1b899eba
bbolker <bbolker@gmail.com>
2015-12-19 16:15:11 -0500

    unbreak refit (rename.response)

--- R/lmer.R
+++ R/lmer.R
@@ -1196,7 +1196,7 @@ refit2.merMod <- function(object,
 ## FIXME DRY: much of copy'n'paste from lmer() etc .. ==> become more modular (?)
 refit.merMod <- function(object,
                          newresp=NULL, formula=NULL, weights=NULL,
-                         ## rename.response=FALSE,
+                         rename.response=FALSE,
                          maxit = 100L,
                          debug=FALSE, ...)
 {
