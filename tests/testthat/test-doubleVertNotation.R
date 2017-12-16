dc75397f07eb3b2fe9b3b76d766e2f7fb2acf542
Ben Bolker <bbolker@gmail.com>
2017-12-16 11:38:50 -0500

    remove spurious commas in test, breaking testthat 2.0.0 tests

--- tests/testthat/test-doubleVertNotation.R
+++ tests/testthat/test-doubleVertNotation.R
@@ -6,12 +6,12 @@ context("testing '||' notation for independent ranefs")
 test_that("basic intercept + slope '||' works", {
 	expect_equivalent(
 		lFormula(Reaction ~ Days + (Days||Subject), sleepstudy)$reTrms,
-		lFormula(Reaction ~ Days + (1|Subject) + (0 + Days|Subject), sleepstudy)$reTrms,
+		lFormula(Reaction ~ Days + (1|Subject) + (0 + Days|Subject), sleepstudy)$reTrms
 	)
 	
 	expect_equivalent(
 		fitted(lmer(Reaction ~ Days + (Days||Subject), sleepstudy)),
-		fitted(lmer(Reaction ~ Days + (1|Subject) + (0 + Days|Subject), sleepstudy)),
+		fitted(lmer(Reaction ~ Days + (1|Subject) + (0 + Days|Subject), sleepstudy))
 	)
 })
 
