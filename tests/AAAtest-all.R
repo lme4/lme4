01533830192f230c1a375466f12611e36a6ee156
Martin Maechler <maechler@r-project.org>
2018-03-22 13:23:58 +0100

    add libPaths() and 'Matrix' package info - to preserve some of my hairs ;-\

--- tests/AAAtest-all.R
+++ tests/AAAtest-all.R
@@ -1,7 +1,16 @@
 if(require("testthat", quietly = TRUE)) {
     pkg   <- "lme4"
     require(pkg, character.only=TRUE, quietly=TRUE)
+    if(getRversion() < "3.5.0") { withAutoprint <- identity ; prt <- print } else { prt <- identity }
+    if(Sys.getenv("USER") %in% c("maechler", "bbolker")) withAutoprint({
+        ## for developers', notably MM's sake:
+        cat(".libPaths():\n"); prt(lP <- .libPaths())
+        cat("entries in .libPaths()[1] :\n"); prt(list.files(lP[1], include.dirs=TRUE))
+        prt(sessionInfo())
+        prt(packageDescription("Matrix"))
+    })
     test_check(pkg)
+    ##======== ^^^
     print(warnings()) # TODO? catch most of these by expect_warning(..)
 } else {
     cat( "package 'testthat' not available, cannot run unit tests\n" )
