9eb765577e7bcd4717097f63f252f97096dc8639
Ben Bolker <bbolker@gmail.com>
2019-02-26 12:15:56 -0500

    quieter allFit tests

--- tests/testthat/test-allFit.R
+++ tests/testthat/test-allFit.R
@@ -6,7 +6,7 @@ L <- load(system.file("testdata", "lme-tst-fits.rda",
 
 context("Show basic allFit results")
 test_that("allFit print/summary is fine", {
-    gm_all <- allFit(fit_cbpp_1)
-    expect_is(print(gm_all), "allFit")
+    gm_all <- allFit(fit_cbpp_1, verbose=FALSE)
+    expect_is(gm_all, "allFit")
     expect_is(summary(gm_all), "summary.allFit")
 })
