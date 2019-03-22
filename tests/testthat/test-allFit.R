5387499e728adf0f95463b012a10893e086e2b94
Ben Bolker <bbolker@gmail.com>
2019-03-22 15:41:09 -0400

    add tests for nloptwrap/allFit (fixes #150)

--- tests/testthat/test-allFit.R
+++ tests/testthat/test-allFit.R
@@ -4,9 +4,18 @@ library("lme4")
 L <- load(system.file("testdata", "lme-tst-fits.rda",
                       package="lme4", mustWork=TRUE))
 
+gm_all <- allFit(fit_cbpp_1, verbose=FALSE)
+
 context("Show basic allFit results")
 test_that("allFit print/summary is fine", {
-    gm_all <- allFit(fit_cbpp_1, verbose=FALSE)
     expect_is(gm_all, "allFit")
     expect_is(summary(gm_all), "summary.allFit")
+})
+
+test_that("nloptwrap switches optimizer correctly", {
+    expect_equal(attr(gm_all[["nloptwrap.NLOPT_LN_BOBYQA"]],"optCtrl"),
+                 list(algorithm = "NLOPT_LN_BOBYQA"))
+    expect_equal(attr(gm_all[["nloptwrap.NLOPT_LN_NELDERMEAD"]],"optCtrl"),
+                 list(algorithm = "NLOPT_LN_NELDERMEAD"))
+
 })
