bfea81939236ab2f9c030c0242e4fdc8523bf89f
Ben Bolker <bbolker@gmail.com>
2019-11-05 09:36:00 -0500

    tighten optimizer tolerances

--- R/utilities.R
+++ R/utilities.R
@@ -1164,7 +1164,7 @@ if(FALSE) {
 nloptwrap <- local({
     ## define default control values in environment of function ...
     defaultControl <- list(algorithm="NLOPT_LN_BOBYQA",
-                           xtol_abs=1e-6, ftol_abs=1e-6, maxeval=1e5)
+                           xtol_abs=1e-8, ftol_abs=1e-8, maxeval=1e5)
     ##
     function(par, fn, lower, upper, control=list(),...) {
         for (n in names(defaultControl))
