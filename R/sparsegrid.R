f426fa2bb3dfe0b48e7e0bbc8c4633ff795301f9
Ben Bolker <bbolker@gmail.com>
2020-09-01 10:33:12 -0400

    avoid use of allPerm_int in GQdk

--- R/sparsegrid.R
+++ R/sparsegrid.R
@@ -6,7 +6,8 @@ GQdk <- function(d=1L, k=1L) {
               0L < (k <- as.integer(k)[1]),
 	      k <= length(GQNd <- GQN[[d]]))## -> GQN, stored in ./sysdata.rda
     tmat <- t(GQNd[[k]])
-    rperms <- lapply(.Call(allPerm_int, seq_len(d) + 1L), function(v) c(1L, v))
+    rperms<- combinat::permn(seq_len(d) + 1L, function(v) c(1L,v))
+    ## rperms <- lapply(.Call(allPerm_int, seq_len(d) + 1L), function(v) c(1L, v))
     dd <- unname(as.matrix(do.call(expand.grid, c(rep.int(list(c(-1,1)), d), KEEP.OUT.ATTRS=FALSE))))
     unname(unique(t(do.call(cbind,
                             lapply(as.data.frame(t(cbind(1, dd))),
