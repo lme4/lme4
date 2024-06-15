3ec90223610a316a098cfe6b98631fd080a00abe
Ben Bolker <bbolker@gmail.com>
2024-06-15 18:11:05 -0400

    still debugging crossprod

--- misc/crossprod.cpp
+++ misc/crossprod.cpp
@@ -4,11 +4,6 @@ typedef Eigen::Map<MatrixXd>  MMat;
 
 MMat V(as<MMat> (RV));
 MMat VtV(as<MMat> (RVtV));
-printf("V.data(): %p\n", (void *) V.data());
-printf("V dims: %ld, %ld\n", V.rows(), V.cols());
-printf("VtV.data(): %p\n", (void *) VtV.data());
-printf("VtV dims: %ld, %ld\n", VtV.rows(), VtV.cols());
 
 VtV.setZero().selfadjointView<Upper>().rankUpdate(V.adjoint());
 
-return wrap(VtV);
