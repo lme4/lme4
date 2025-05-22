b4bafee151b4e61b308386eb750f4bfc6710c17d
Ben Bolker <bbolker@gmail.com>
2025-05-22 10:07:21 -0400

    don't use eval.parent() in shims

--- R/reformulas_imports.R
+++ R/reformulas_imports.R
@@ -1,21 +1,21 @@
 ## transient patches for moving formula manipulation machinery to lme4 without breaking downstream packages
 subbars <- function(...) {
   rlang::warn("the subbars() function has moved to the reformulas package. Please update your imports, or ask an upstream package maintainter to do so.", .frequency = "once", .frequency_id = "subbars")
-  eval.parent(reformulas::subbars(...))
+  reformulas::subbars(...)
 }
 
 nobars <- function(...) {
   rlang::warn("the nobars() function has moved to the reformulas package. Please update your imports, or ask an upstream package maintainter to do so.", .frequency = "once", .frequency_id = "nobars")
-  eval.parent(reformulas::nobars(...))
+  reformulas::nobars(...)
 }
 
 findbars <- function(...) {
   rlang::warn("the findbars() function has moved to the reformulas package. Please update your imports, or ask an upstream package maintainter to do so.", .frequency = "once", .frequency_id = "findbars")
-  eval.parent(reformulas::findbars(...))
+  reformulas::findbars(...)
 }
 
 mkReTrms <- function(...) {
   rlang::warn("the mkReTrms() function has moved to the reformulas package. Please update your imports, or ask an upstream package maintainter to do so.", .frequency = "once", .frequency_id = "mkReTrms")
-  eval.parent(reformulas::mkReTrms(...))
+  reformulas::mkReTrms(...)
 }
 
