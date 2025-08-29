3041dcc573a6cd43dd7693f935c1be08a0d67c74
Ben Bolker <bbolker@gmail.com>
2025-08-29 09:25:25 -0400

    clean up ecoreg

--- misc/ecoreg.R
+++ misc/ecoreg.R
@@ -1,23 +1,26 @@
 download.file("https://github.com/bbolker/mmd_utils/raw/refs/heads/master/ecoreg.rds", dest = "ecoreg.rds")
 ee <- readRDS("ecoreg.rds")
 
-form <- mmamm_log ~
+form_full <- mmamm_log ~
     (NPP_log_sc + Feat_log_sc + NPP_cv_sc + Feat_cv_sc)^2 +
     (NPP_log_sc + Feat_log_sc + NPP_cv_sc + Feat_cv_sc | biome)
 
-library(lme4)
-ee1 <- lmer(form, ee, REML = FALSE)
-isSingular(ee1)
-
-
-form5 <- mmamm_log ~
+form_diag <- mmamm_log ~
   (NPP_log_sc + Feat_log_sc + NPP_cv_sc + Feat_cv_sc)^2 +
   diag(NPP_log_sc + Feat_log_sc + NPP_cv_sc + Feat_cv_sc | biome)
 
-lmer(form5, ee, REML = FALSE)
-
-form6 <- mmamm_log ~
+form_cs <- mmamm_log ~
   (NPP_log_sc + Feat_log_sc + NPP_cv_sc + Feat_cv_sc)^2 +
   cs(NPP_log_sc + Feat_log_sc + NPP_cv_sc + Feat_cv_sc | biome)
 
-mm <- lmer(form6, ee, REML = FALSE)
+form_list <- namedList(form_full, form_diag, form_cs)
+lmer_list <- lapply(form_list, \(f) try(lmer(f, ee, REML = FALSE)))
+
+library(glmmTMB)
+glmmTMB_list <- lapply(form_list, \(f) try(glmmTMB(f, ee, REML = FALSE)))
+
+lapply(glmmTMB_list, VarCorr)
+
+lapply(lmer_list, \(m) try(VarCorr(m)))
+try(VarCorr(lmer_list[[1]]))  ## this is broken and wasn't before?
+
