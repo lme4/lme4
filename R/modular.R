3a4bc87942c880fb56c3a8adb883e8a330e0fd77
annahuynhly <lya19@mcmaster.ca>
2025-10-12 20:54:24 -0400

    added vignette

--- R/modular.R
+++ R/modular.R
@@ -139,7 +139,8 @@ checkScaleX <- function(X,  kind="warning", tol=1e3) {
     if (any(c(logcomp,logsd) > log(tol))) {
         wmsg <- "Some predictor variables are on very different scales:"
         if (kind %in% c("warning","stop")) {
-            wmsg <- paste(wmsg, "consider rescaling")
+            msg2 <- "\nYou may also use (g)lmerControl(autoscale = TRUE) to avoid numerical stability."
+            wmsg <- paste(wmsg, "consider rescaling.", msg2)
             switch(kind,
                    "warning" = warning(wmsg, call.=FALSE),
                    "stop" = stop(wmsg, call.=FALSE))
