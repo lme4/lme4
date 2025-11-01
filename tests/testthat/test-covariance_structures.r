9268ea55c1ab87974466f13018bd238e5dcbb50d
annahuynhly <lya19@mcmaster.ca>
2025-11-01 16:19:47 -0400

    adding more unit tests; slight issue with covariance.us that is commented out

--- tests/testthat/test-covariance_structures.r
+++ tests/testthat/test-covariance_structures.r
@@ -68,3 +68,156 @@ test_that("unit test for unstructured covariances", {
     expect_equal(getVC(x.us)$ccomp, ccomp_test)
   }
 })
+
+test_that("unit tests for diagonal covariances", {
+  
+  for(i in seq_len(100L)){
+    nc <- sample(0:10, 1L)
+    hom_test <- sample(c(T, F), size = 1)
+    
+    x.di <- new("Covariance.diag", nc = nc, hom = hom_test, simulate = TRUE)
+    
+    ## Basic sanity tests
+    expect_s4_class(x.di, "Covariance.diag")
+    expect_true(validObject(x.di))
+    
+    ## par should be the same as theta
+    expect_equal(getPar(x.di), getTheta(x.di))
+    
+    ## checking for specific par/theta lengths
+    if(hom_test){
+      expect_equal(getParLength(x.di), if (nc > 0L) 1 else 0)
+    } else {
+      expect_equal(getParLength(x.di), if (nc > 0L) nc else 0)
+    }
+    
+    expect_equal(getParLength(x.di), getThetaLength(x.di))
+    expect_equal(length(getThetaIndex(x.di)), 
+                 getThetaIndexLength(x.di))
+    
+    expect_equal(getLambdat.dp(x.di), rep(1L, nc))
+    
+    ## Testing getUpper() and getLower() in a different manner
+    if(hom_test){
+      expect_equal(getUpper(x.di), if(nc > 0L) Inf else numeric(0))
+      expect_equal(getLower(x.di), if(nc > 0L) 0 else numeric(0))
+    } else {
+      expect_equal(getUpper(x.di), if(nc > 0L) rep(Inf, nc) else numeric(0))
+      expect_equal(getLower(x.di), if(nc > 0L) rep(0, nc) else numeric(0))
+    }
+    
+    ## Testing getLambdat.i in a different manner
+    expect_equal(getLambdat.i(x.di), 
+                 if(nc > 0L) 0:(nc-1) else integer(0))
+    
+    ## Testing getLambda in a different manner
+    expect_equal(getLambda(x.di),
+                 diag(nc) * getPar(x.di))
+    
+    ## Testing getVC
+    expect_equal(getVC(x.di)$ccomp, numeric(0))
+    expect_equal(getVC(x.di)$vcomp, getPar(x.di))
+  }
+})
+
+test_that("unit tests for compound symmetry covariances", {
+  
+  for(i in seq_len(100L)){
+    nc <- sample(0:10, 1L)
+    hom_test <- sample(c(T, F), size = 1)
+    
+    x.cs <- new("Covariance.cs", nc = nc, hom = hom_test, simulate = TRUE)
+    
+    ## Basic sanity tests
+    expect_s4_class(x.cs, "Covariance.cs")
+    expect_true(validObject(x.cs))
+    
+    ## par should be the same as vcomp and ccomp from getVC
+    expect_equal(getPar(x.cs), c(getVC(x.cs)$vcomp, getVC(x.cs)$ccomp))
+    
+    ## testing for getTheta via the structure of getLambda
+    ## (odd way of looking at things, but it nicely compares the relationship
+    ## between these two functions.)
+    lam_mat <- getLambda(x.cs)
+    collected <- 1
+    if(nc > 0L){
+      if(hom_test){ ## for homogenous structures only
+        collect <- numeric(nc*2-1)
+        collected <- 1
+        for(i in 1:nc){
+          if(i != nc){
+            collect[collected:(collected+1)] <- lam_mat[i:(i+1),i]
+          } else {
+            collect[collected:collected] <- lam_mat[i,i]
+          }
+          collected = collected + 2
+        }
+      } else { ## for non-homogenous structures
+        grab <- nc
+        collect <- numeric((nc * (nc+1)/2))
+        for(i in 1:nc){
+          if(i != nc){
+            collect[collected:(collected + (nc-i))] <- lam_mat[i:nc,i]
+          } else {
+            collect[collected:collected] <- lam_mat[i,i]
+          }
+          collected = collected + (nc - i + 1)
+        }
+      }
+    } else {
+      collect = numeric(0)
+    }
+    expect_equal(getTheta(x.cs), collect)
+    
+    ## Testing getParLength
+    if(hom_test){ ## for homogenous, should be no more than 2
+      if(nc <= 2L){
+        expect_equal(getParLength(x.cs), nc)
+      } else {
+        expect_equal(getParLength(x.cs), 2L)
+      }
+    } else { ## Comparing length to vcomp and ccomp 
+      expect_equal(getParLength(x.cs),
+                   sum(lengths(getVC(x.cs)[c("vcomp", "ccomp")])))
+    }
+    
+    expect_equal(getParLength(x.cs), length(getPar(x.cs)))
+    # TODO: the test below currently fails
+    #expect_equal(getThetaLength(x.cs), length(getTheta(x.cs)))
+    
+    ## Testing getThetaLength
+    expect_equal(length(getThetaIndex(x.cs)), 
+                 getThetaIndexLength(x.cs))
+    if(hom_test){
+      # TODO: the test below currently fails
+      #expect_equal(getThetaLength(x.cs), if(nc > 0L) (nc*2 - 1) else 0)
+    } else {
+      expect_equal(getThetaLength(x.cs), (nc * (nc+1)/2))
+    }
+  
+    expect_equal(getLambdat.dp(x.cs), if(nc > 0L) 1:nc else integer(0))
+    
+    ## Testing getUpper() and getLower()
+    if(nc > 1L){
+      if(hom_test) {
+        expect_equal(getUpper(x.cs), c(Inf, 1))
+        expect_equal(getLower(x.cs), c(0.0, -1/(nc-1)))
+      } else {
+        expect_equal(getUpper(x.cs), c(rep(Inf, nc), 1))
+        expect_equal(getLower(x.cs), c(rep(0.0, nc), -1/(nc-1)))
+      }
+    } else if (nc == 1L) {
+      expect_equal(getUpper(x.cs), Inf)
+      expect_equal(getLower(x.cs), 0)
+    } else {
+      expect_equal(getUpper(x.cs), numeric(0))
+      expect_equal(getLower(x.cs), numeric(0))
+    }
+    
+    ## Testing getLambdat.i in a different manner
+    expect_equal(getLambdat.i(x.cs), 
+        (row(matrix(0, nc, nc)) - 1)[upper.tri(matrix(0, nc, nc), diag = TRUE)])
+    
+  }
+})
+
