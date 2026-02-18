ad269bcee3e3db8fc2aff7d971e3f5c007b1a09f
Ben Bolker <bbolker@gmail.com>
2026-02-18 12:07:39 -0500

    version bump [run ci]

--- misc/varcorr_print.R
+++ misc/varcorr_print.R
@@ -6,10 +6,20 @@ library(glmmTMB)
 
 library(lme4)
 set.seed(20260217)
-dd <- expand.grid(f = factor(1:30), g = factor(1:30))
+dd <- expand.grid(f = factor(1:100), g = factor(1:30))
 dd$y <- glmmTMB::simulate_new(~ 1 + ar1(0 + f | g),
                       newdata = dd,
                       newparams = list(beta = 0, theta = c(0,0)),
                       family = gaussian)[[1]]
-        
+
+system.time(
+  m <- lmer(y ~ 1 + ar1(0 + f | g), data = dd)
+)
+
+##   user  system elapsed 
+## 78.425 152.311  15.291 
+
+## note covmat is still full size ...
+object.size(VarCorr(m))
+str(VarCorr(m))
 
