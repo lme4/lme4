bb891d59f26719fd9fca96010f0a8f38479aaa75
Ben Bolker <bbolker@gmail.com>
2026-04-18 15:32:53 -0400

    working on GH 962

--- misc/GH_962.R
+++ misc/GH_962.R
@@ -7,6 +7,8 @@ form2 <- update(form, . ~ . - ar1(ftime + 0 | id_cluster) +
 form3 <- update(form, . ~ . - ar1(ftime + 0 | id_cluster) +
                         (1|id_cluster/ftime))
 
+form4 <- update(form, . ~ . - (1 | id_individual))
+
 library(glmmTMB)
 library(lme4)
 stopifnot(packageVersion("lme4") >= "2.0.2")
@@ -24,8 +26,64 @@ dd$out_bin_1 <- simulate_new(form[-2], newdata = dd,
 ## looks OK after nAGQ0: environment(devfun)$pp$beta(1)
 ## but not after nAGQ1
 
+## looks like it's within optimizeGlmer -> devfun() -> pwrssUpdate() ?
+## inside .Call(glmerLaplace, ...) ?
+
+## + + *** pwrssUpdate step 0
+## pdev=1351.8; delu_min: -1.52241; delu_max: 1.69336; delb_min: 0; delb_max: 0
+## *** pwrssUpdate step 1
+## pdev=1351.8; delu_min: -1.52241; delu_max: 1.69336; delb_min: 0; delb_max: 0
+## [1] 1638.19
+
+## ?? set debug flag internally?
+## try git bisect ... ??
+## narrow down cases where this happens? (is it true for *all* 
+
+## Not universal. e.g.
+
+gm.cs <- glmer(use ~ age + urban + cs(1 + urban | district),
+               data = Contraception,
+               family = binomial)
+fixef(gm.cs)  ## OK
+
+gm.ar1_0 <- glmer(use ~ age + urban + ar1(0 + urban | district),
+               data = Contraception,
+               family = binomial)
+colnames(getME(gm.ar1_0, "X"))
+getME(gm.ar1_0, "beta")  ## only two values ???
+
+try(fixef(gm.ar1_0))
+## Error in attributes(.Data) <- c(attributes(.Data), attrib) (from lmer.R#971) : 
+##   'names' attribute [3] must be the same length as the vector [2]
+
+
+
+## but ...
+gm.ar1 <- glmer(use ~ urban + ar1(0 + factor(age) | district),
+               data = Contraception,
+               family = binomial)
+## uh-oh
+fixef(gm.ar1)
+
 ## still not sure what's going on ...
 ## cross-check with other forms?
+
+## from a833370f0b10ad221b43c400fa1 (pre-2.0), for comparison
+## devfun for glmer/nAGQ > 0
+olddevfun <- function(pars) {
+  ## pp$setDelu(rep(0, length(pp$delu)))
+  resp$setOffset(baseOffset)
+  resp$updateMu(lp0)
+  pp$setTheta(as.double(pars[dpars])) # theta is first part of pars
+  spars <- as.numeric(pars[-dpars])
+  offset <- if (length(spars)==0) baseOffset else baseOffset + pp$X %*% spars
+  resp$setOffset(offset)
+  p <- pwrssUpdate(pp, resp, tol=tolPwrss, GQmat=GQmat,
+                   compDev=compDev, grpFac=fac, maxit=maxit, verbose=verbose)
+  resp$updateWts()
+  p
+}
+
 lme4_1 <- glmer(form, data = dd, family = binomial)
 fixef(lme4_1)
 lme4_1@optinfo$val
@@ -40,6 +98,10 @@ lme4_2@optinfo$val
 lme4_3 <- update(lme4_1, form = form3)
 fixef(lme4_3)
 lme4_3@optinfo$val
+
+## ar1 only, no individual RE
+lme4_4 <- update(lme4_1, form = form4)
+fixef(lme4_4)
 
 ## only happens for 'new-style' (structured); something happens
 ## (or fails to happen) in the parameter mapping step?
