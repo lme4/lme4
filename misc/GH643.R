78d3cc774aaebb4481aecbe45cba3290a9eb45a5
Ben Bolker <bbolker@gmail.com>
2026-08-02 13:35:51 -0400

    working on glmer vignette (troubleshooting)

--- misc/GH643.R
+++ misc/GH643.R
@@ -1,6 +1,15 @@
 library(lme4)
 library(glmmTMB)
 library(GLMMadaptive)
+library(glmmrBase) ## ?? how do I get this to do Laplace approx?
+## check goodness-of-fit/model assumptions
+library(DHARMa)
+library(performance)
+## extract info
+library(broom.mixed)
+library(parameters)
+library(dplyr)
+library(purrr)
 
 ## what is going on here?  Try with simulated data?
 data(epil2)
@@ -13,7 +22,10 @@ mod_GA <- mixed_model(y ~ trt,
                       random = ~ 1|subject, data=epil2_red,
                       family=fam, control = list(nAGQ=1))
 
-AIC(mod_glmer, mod_glmmTMB, mod_GA)   # very different AIC
+mod_list <- list(glmer=mod_glmer, glmmTMB = mod_glmmTMB, GA=mod_GA)
+bbmle::AICtab(mlist = mod_list)
+
+## AIC() gives 
 ## Warning message (ignorable):
 ## In AIC.default(mod_glmer, mod_glmmTMB, mod_GA) :
 ##   models are not all fitted to the same number of observations
@@ -23,11 +35,23 @@ AIC(mod_glmer, mod_glmmTMB, mod_GA)   # very different AIC
 ## VarCorr ???
 ## Gamma should report VarCorr as the same as theta[[1]] (unscaled)?
 ##  double-check 'fix'
-mod_list <- list(glmer=mod_glmer, glmmTMB = mod_glmmTMB, GA=mod_GA)
+
+## GA gives very different answers
 sapply(mod_list, function(x) unlist(fixef(x)[1:2]))
 
+check_model(mod_list[[1]])
+check_model(mod_list[[2]])
+plot(simulateResiduals(mod_list[[1]]))
+
 ## very different -- if we believe these then
-## glmer >> glmmTMB >> GA
+## glmer >> glmmTMB >> GAmodels
+
+(purrr::map_dfr(mod_list,
+               \(x) tidy(x, effects =  c("fixed", "ran_pars")),
+               .id = "model")
+  |> select(term, estimate, std.error)
+)
+  
 sapply(mod_list, function(x) -1*c(logLik(x)))
 
 rssq <- function(x) sum(residuals(x, type = "pearson")^2)
@@ -127,3 +151,35 @@ hist(as.matrix(simulate(glmer_mod, nsim = 1))[,1],
 hist(as.matrix(simulate(glm_mod, nsim = 1))[,1],
      main="glm",xlab="",breaks=seq(0,1.5,0.05))
 hist(data$y,main="original distribution",xlab="",breaks=seq(0,1.5,0.05))
+
+
+### simulation exercise
+
+par0 <- list(beta = c(2, -0.5), betadisp = 1.5, theta = -0.2)
+sim_fun <- function(params = par0) {
+  epil2$y <- simulate_new(form[-2], family = fam,
+                          newparams = params,
+                          newdata = epil2)[[1]]
+  subset(epil2, select = c(y, trt, subject))
+}
+
+sum_fun <- function(data = sim_fun(par0), params = par0) {
+  mod_list <- suppressWarnings(list(glmer = glmer(form, data=data, family=fam),
+                   glmmTMB = glmmTMB(form, data=data, family=fam)))
+  results_par <- purrr::map_dfr(mod_list,
+                 \(x) tidy(x, effects =  c("fixed", "ran_pars"), conf.int = TRUE,
+                           conf.level = 0.9),
+                 .id = "model") |>
+    dplyr::select(model, term, estimate, conf.low, conf.high)
+  results_sum <- purrr::map_dfr(mod_list, glance, .id = "model") |>
+    select(sigma, logLik, deviance)
+  list(par = results_par, sum  = results_sum, true = params)
+}
+
+library(lme4)
+data(epil2, package = "glmmTMB")
+epil2_red <- subset(epil2, y>0)
+form <- y ~ trt + (1|subject)
+fam <- Gamma(link = "log")
+mod_glmer <- glmer(form, data=epil2_red, family=fam)
+broom.mixed::tidy(mod_glmer, conf.int = TRUE)
